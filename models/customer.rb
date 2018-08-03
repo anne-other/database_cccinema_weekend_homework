033d4f2f58699c41f93e05b819baebafdbb9d074
anne-other <anne.c.ince@gmail.com>
2018-08-03 21:06:56 +0100

    adding update function to customer

--- models/customer.rb
+++ models/customer.rb
@@ -18,6 +18,12 @@ class Customer
     @id = customer[0]['id'].to_i()
   end
 
+  def update()
+    sql = "UPDATE customers SET (name, funds) = ($1, $2) WHERE id = $3"
+    values = [@name, @funds, @id]
+    SqlRunner.run(sql, values)
+  end
+
   def self.delete_all()
     sql = "DELETE FROM customers"
     SqlRunner.run(sql)
@@ -28,5 +34,6 @@ class Customer
     customers = SqlRunner.run(sql)
     return customers.map { |customer| Customer.new(customer)}
   end
+
 
 end
