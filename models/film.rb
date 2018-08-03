a8f553097994bf2d0d1f079e84455462f3059767
anne-other <anne.c.ince@gmail.com>
2018-08-03 20:57:25 +0100

    returning all films via ruby

--- models/film.rb
+++ models/film.rb
@@ -23,4 +23,10 @@ class Film
     SqlRunner.run(sql)
   end
 
+  def self.all()
+    sql = "SELECT * FROM films"
+    films = SqlRunner.run(sql)
+    return films.map { |film| Film.new(film)}
+  end
+
 end
