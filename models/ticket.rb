6461ea111fec3dc6be8d7b262df51e32dcdd1957
anne-other <anne.c.ince@gmail.com>
2018-08-03 18:18:13 +0100

    adding tickets to table

--- models/ticket.rb
+++ models/ticket.rb
@@ -10,4 +10,11 @@ class Ticket
     @film_id = options['film_id']
   end
 
+  def save()
+    sql = "INSERT INTO tickets (customer_id, film_id) VALUES ($1, $2) RETURNING id"
+    values = [@customer_id, @film_id]
+    ticket = SqlRunner.run(sql, values)
+    @id = ticket[0]['id'].to_i()
+  end
+
 end
